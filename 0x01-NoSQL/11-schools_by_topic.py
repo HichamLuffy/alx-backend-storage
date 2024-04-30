1e86a307845445485013491240bf9f37b198366a
HichamLuffy <imalexvssteve2020@gmail.com>
2024-04-30 20:54:00 +0100

    task 10 and 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+mod doc
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    func doc
+    """
+    return mongo_collection.find({"topics": topic})
