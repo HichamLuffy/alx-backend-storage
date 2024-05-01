c20ad66ff09b7c41217176356e7a833602edf0e2
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-01 13:35:35 +0100

    fix style

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -47,6 +47,7 @@ def replay(method: Callable) -> None:
 
 class Cache:
     """doc class cache"""
+
     def __init__(self):
         """init"""
         self._redis = redis.Redis()
@@ -59,19 +60,19 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
-    
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, bytes, int, float]:
         """get"""
         value = self._redis.get(key)
         if value is not None and fn is not None:
             value = fn(value)
         return value
-    
+
     def get_str(self, key: str) -> str:
         """get str"""
         return self.get(key, fn=lambda x: x.decode('utf-8'))
-    
+
     def get_int(self, key: str) -> int:
         """get int"""
         return self.get(key, fn=int)
-
