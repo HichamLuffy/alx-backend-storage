b19cd7ee26e4c1064888554cf1cb2d432b355222
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-01 13:07:03 +0100

    task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""exercise"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key
